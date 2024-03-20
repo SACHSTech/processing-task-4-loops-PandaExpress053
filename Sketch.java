51c11309bd0c640d2c5aef51f510d12ae7bacf88
PandaExpress053 <ethan.fung25@ycdsbk12.ca>
2024-03-20 11:22:39 -0400

    modified:   Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -23,32 +23,32 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	// sample code, delete this stuff
+    // Changes outlining colour to black
     stroke(0);
+
     // Q1
-    //Vertical Line
-    for(int intX = 1; intX < 10; intX++){
-      strokeWeight(2);
+    // Vertical Line
+    for(int intVerticalLine = 1; intVerticalLine < 10; intVerticalLine++){
+      strokeWeight(1);
       fill(0);
-      line(intX * (200 / 10), 0, intX * (200 / 10), height / 2);
+      line(intVerticalLine * (200 / 10), 0, intVerticalLine * (200 / 10), height / 2);
     }
-    //Horizontal Line
-    for(int x = 10; x > 0; x--){
-      strokeWeight(2);
+    // Horizontal Line
+    for(int intHorizontalLine = 10; intHorizontalLine > 0; intHorizontalLine--){
+      strokeWeight(1);
       fill(0);
-      line(0, x * (200 / 10), width/2, x * (200 / 10));
+      line(0, intHorizontalLine * (200 / 10), width/2, intHorizontalLine * (200 / 10));
     }
 
-    //Q2
+    // Q2
     for(int intRow = 1; intRow < 6; intRow++){
       for(int intColumn = 1; intColumn < 6; intColumn++){
         fill(255, 0, 0);
-        ellipse((intColumn) * (200 / 6) + 200, intRow * (200 / 6), 15, 15);
-        //System.out.println(column + " " + row);
+        ellipse((intColumn) * (200 / 6) + 200, intRow * (200 / 6), 22, 22);
       }
     }
 
-    //Q3
+    // Q3
     for(int intY = 0; intY < 200; intY++){
       for(int intX = 0; intX < 200; intX++){
         int intGreyScale =  (int) (1.275 * intX);
@@ -56,24 +56,23 @@ public class Sketch extends PApplet {
         point(intX, intY + 200);
       }
     }
-    
-    //Q4    
+
+    // Q4    
     for (int intPetalNum = 0; intPetalNum < 8; intPetalNum++){
       fill(255, 180, 0);
       pushMatrix();
       translate(width * 0.75f, height * 0.75f);
 
-      //Rotates in a circle
+      // Rotates petals in a circle
       rotate(-intPetalNum * (PI / 4));
       translate ((float) (width / 12), 0);
       ellipse(0, 0, (float)(width / 6), (float)(height / 22));
       popMatrix();
 
     }
+    // Middle Circle of FLower
     strokeWeight(0);
     fill(32, 92, 34);
     ellipse(width * 0.75f, height * 0.75f, 30, 30);
   }
-  
-  // define other methods down here.
 }
